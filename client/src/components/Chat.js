509f15072f7ebc0adcb97e319f373c70dfe2e1ad
John Wesly <jwesly20@gmail.com>
2023-03-16 19:48:10 +0530

    firstcommit

---
+++ client/src/components/Chat.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+import InfoBar from './InfoBar';
+import Input from './Input';
+import Messages from './Messages';
+
+let socket;
+
+const Chat = ({ location }) => {
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+    const [users, setUsers] = useState([]);
+    const [message, setMessage] = useState('');
+    const [messages, setMessages] = useState([]);
+    const ENDPOINT = process.env.REACT_APP_ENDPOINT;
+
+    useEffect(() => {
+        const { name, room } = queryString.parse(location.search);
+
+        socket = io(ENDPOINT);
+
+        setName(name);
+        setRoom(room);
+
+        socket.emit('join', { name, room }, (error) => {
+            if(error) {
+                alert(error);
+                window.location='/';
+            }
+        });
+    }, [ENDPOINT, location.search]);
+
+    useEffect(() => {
+        socket.on('message', message => {
+            setMessages(messages => [ ...messages, message ]);
+        });
+        socket.on('roomData', ({ users }) => {
+            setUsers(users);
+        })
+    }, []);
+
+    const sendMessage = (event) => {
+        event.preventDefault();
+        if(message) {
+            socket.emit('sendMessage', message, () => setMessage(''));
+        }
+    };
+
+    return (
+        <div className="container">
+            <InfoBar room={room} users={users} />
+            <Messages messages={messages} name={name} />
+            <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
+        </div>
+    );
+}
+
+export default Chat;
---
+++ client/src/components/InfoBar.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import closeIcon from '../icons/closeIcon.png';
+import onlineIcon from '../icons/onlineIcon.png';
+
+const OnlineList = ({ users }) => {
+    let _users = [];
+    users.forEach((user) => _users.push(user.name));
+    return _users.join(', ');
+};
+
+const InfoBar = ({ room, users }) => (
+    <div className="infoBar" title="Room">
+        <div className="leftInnerContainer">
+            <img className="onlineIcon" src={onlineIcon} alt="online"/>
+            <h3>{room}<br/>(<OnlineList users={users}/>) </h3>
+        </div>
+        <div className="rightInnerContainer" title="Logout">
+            <a href="/"><img src={closeIcon} alt="close"/></a>
+        </div>
+    </div>
+);
+
+export default InfoBar;
